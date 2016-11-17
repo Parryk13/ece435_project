0287c2a2854c8111d1691280015f78080d77286d
Kyle Parry <parryk13@gmail.com>
2016-11-17 13:41:51 -0500

    made changes

--- battleship.c
+++ battleship.c
@@ -14,56 +14,91 @@ int* find points(char *input){
 
         if(strlen(input)==3){
 
-            for(i=0;i<strlen(buffer);i++){
+            for(i=0;i<strlen(input);i++){
                 
                 if(i==0){
-                    switch (buffer[i]) {
+                    switch (input[i]) {
+                       /* case '0':
+                            points[0]=0;
+                            break;*/
                         case '1':
-                            points[0]=1;
+                            points[0]=0;
                             break;
                         case '2':
+                            points[0]=1;
+                            break
+                        case '3':
                             points[0]=2;
                             break;
-                        case '3':
-                            points[0]=3;
-                            break
                         case '4':
-                            points[0]=4;
+                            points[0]=3;
                             break;
                         case '5':
-                            points[0]=5;
+                            points[0]=4;
                             break;
                         case '6':
-                            points[0]=6;
-                            break;
+                            points[0]=5;
+                            break
                         case '7':
-                            points[0]=7;
+                            points[0]=6;
                             break
                         case '8':
-                            points[0]=8;
-                            break
+                            points[0]=7;
+                            break;
                         case '9':
-                            points[0]=9;
+                            points[0]=8;
                             break;
                         default:
+
                             points[0]=99;
                             break;
 
                     }
               }
-              else if(i==1&&buffer[i]!='-'){
-                                 
-                                     
+              else if(i==1&&input[i]!='-'){
+                           points[0]=99;
+                           points[1]=99;
+                           return points;
                     }
 
               }
               else if(i==2){
                     switch (buffer[i]) {
-
-
+                        case 'a':
+                            points[1]=0;
+                            break;
+                        case 'b':
+                            points[1]=1;
+                            break;
+                        case 'c':
+                            points[1]=2;
+                            break;
+                        case 'd':
+                            points[1]=3;
+                            break;
+                        case 'e':
+                            points[1]=4;
+                            break;
+                        case 'f':
+                            points[1]=5;
+                            break;
+                        case 'g':
+                            points[1]=6;
+                            break
+                        case 'h':
+                            points[1]=7;
+                            break;
+                        default:
+                            points[1]=99;
+                            break;
                     }
 
               }
-
-
+         }  
+    }
+    else {
+        points[0]=99;
+        points[1]=99;
+    }
+    return points;            
 }
