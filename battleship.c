dd4b9562d181b6d3aaadbc1e4ed4faa055513fee
Kyle Parry <parryk13@gmail.com>
2016-11-17 13:17:29 -0500

    made battleship file

---
+++ battleship.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+#include <sys/socket.h>
+
+#include <netdb.h>
+
+int* find points(char *input){
+        int i;
+        int points[2];
+
+        if(strlen(input)==3){
+
+            for(i=0;i<strlen(buffer);i++){
+                
+                if(i==0){
+                    switch (buffer[i]) {
+                        case '1':
+                            points[0]=1;
+                            break;
+                        case '2':
+                            points[0]=2;
+                            break;
+                        case '3':
+                            points[0]=3;
+                            break
+                        case '4':
+                            points[0]=4;
+                            break;
+                        case '5':
+                            points[0]=5;
+                            break;
+                        case '6':
+                            points[0]=6;
+                            break;
+                        case '7':
+                            points[0]=7;
+                            break
+                        case '8':
+                            points[0]=8;
+                            break
+                        case '9':
+                            points[0]=9;
+                            break;
+                        default:
+                            points[0]=99;
+                            break;
+
+                    }
+              }
+              else if(i==1&&buffer[i]!='-'){
+                                 
+                                     
+                    }
+
+              }
+              else if(i==2){
+                    switch (buffer[i]) {
+
+
+                    }
+
+              }
+
+
+}
