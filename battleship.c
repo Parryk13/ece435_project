3fb7102b7336bfe3091ea8b77d0391ee652f611c
kyle Parry <parryk13@gmail.com>
2016-12-06 17:11:51 -0500

    weird seg problem

--- battleship.c
+++ battleship.c
@@ -174,7 +174,7 @@ int setship(char board[9][9], char ship)
   //printf("its not this bs\n");
   //prompt for direction
   //printf("Choose a direction (u for up, d for down, l for left, or r for right): ");
-  printf("super weird?\n")
+  printf("super weird?\n");
   scanf("%c", direction);
   if(*direction != 'u' && *direction != 'd' && *direction != 'l' && *direction != 'r')
   {
